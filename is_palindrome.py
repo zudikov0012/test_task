c4230952f10bbf6575fa86b26ac6be91135a7616
zudikov0012 <67049754+zudikov0012@users.noreply.github.com>
2022-02-19 04:21:34 +0200

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -10,9 +10,12 @@ Test task
 """
 # TODO Import the necessary libraries
 
+import  re
 
 # TODO Write here is_palindrome function 
 
-def is_palindrome():
-    
-    return None
+def is_palindrome(a):
+    my_string = str(a).lower().replace(' ', '')
+    my_string = re.sub(r'[^a-z0-9]', '', my_string)
+    reverse_string = my_string[::-1]
+    return my_string == reverse_string
